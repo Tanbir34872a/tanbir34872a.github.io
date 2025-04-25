18c66fa8a8e0718b605ff7903754c2cc346b04c9
Tanbir Yousuf <yousuf.tanbir@gmail.com>
2025-04-25 16:09:50 +0600

    New style

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
