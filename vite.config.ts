f885ab8177e390cfedab8a0bf53901ee4607a729
Tanbir Yousuf <yousuf.tanbir@gmail.com>
2025-04-26 20:56:08 +0600

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
